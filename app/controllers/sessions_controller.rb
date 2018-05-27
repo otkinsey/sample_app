f8bef0e8b1a63884c055289dda0bf88dbec85745
okoa kinsey <okoakinsey@okoas-MacBook-Pro.local>
2018-05-27 09:59:42 -0400

    complete chapter 9.1.4 having problem with integration test for login/logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,12 @@
 class SessionsController < ApplicationController
   def new
   end
-  
+
   def create
     user = User.find_by(email: params[:session][:email].downcase )
     if user && user.authenticate( params[:session][:password])
         log_in user
+        remember user
         redirect_to user
       else
         flash.now[:danger] = "Danger/Warning message!"
@@ -14,5 +15,8 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    log_out if logged_in?
+    redirect_to root_url
   end
+
 end

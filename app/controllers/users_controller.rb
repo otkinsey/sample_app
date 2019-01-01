de8df222336dafae93d0e413bcbbcda2b9b1da0f
okoa kinsey <okoakinsey@okoas-MacBook-Pro.local>
2019-01-01 14:17:10 -0500

    section 11.3.3 complete, start at section 11.4: activation email must be tested (see exercises below list 11.38)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,7 +22,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      UserMailer.account_activation(@user).deliver_now
+      @user.send_activation_email
       flash[:info] = "Please check your email activate your account"
       redirect_to root_url
     else
@@ -69,4 +69,6 @@ class UsersController < ApplicationController
     def admin_user
       redirect_to(root_url) unless current_user.admin?
     end
+
+
 end
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,6 +1,51 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
+  def setup
+    ActionMailer::Base.deliveries.clear
+  end
+
+  test "invalid signup information" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, params: { user: { name: '',
+                                         email: 'user@invalid',
+                                         password: 'foo',
+                                         password_confirmation: 'bar' } }
+    end
+    assert_template 'users/new'
+    assert_select 'div.error_explanation'
+    assert_select 'div.field_with_errors'
+  end
+
+  test "valid signup information with account activation" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name: "Example User",
+                                         email: "example@example.com",
+                                         password: "password",
+                                         password_confirmation: "password"} }
+      end
+      assert_equal 1, ActionMailer::Base.deliveries.size
+      user = assigns(:user)
+      assert_not user.activated?
+      # try to login before activation
+      log_in_as(user)
+      assert_not user.activated?
+      # try to login invalid activataion
+      get edit_account_activation_path("invalid token", email: user.email)
+      assert_not logged_in?
+      # try to login wrong email
+      get edit_account_activation_path(user.activation_token, email: 'wrong')
+      assert_not logged_in?
+      # try to login valid activation token
+      get edit_account_activation_path(user.activation_token, email: user.email)
+      assert user.reload.activated?
+      follow_redirect!
+      assert_template 'users/show'
+      assert logged_in?
+    end
+
   test "valid information" do
     get signup_path
     assert_no_difference 'User.count' do
@@ -10,13 +55,13 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_select 'div.error_explanation'
   end
 
-  test "valid signup information" do
-    get signup_path
-    assert_difference 'User.count' do
-      post users_path, params: { user:{ name:"Some One", email:"next@next.com", password:"pass1234", password_confirmation:"pass1234"} }
-    end
-    follow_redirect!
-    # assert_template 'users/show'
-    # assert_not flash.empty?
-  end
+  # test "valid signup information" do
+  #   get signup_path
+  #   assert_difference 'User.count' do
+  #     post users_path, params: { user:{ name:"Some One", email:"next@next.com", password:"pass1234", password_confirmation:"pass1234"} }
+  #   end
+  #   follow_redirect!
+  #   assert_template 'users/show'
+  #   assert_not flash.empty?
+  # end
 end

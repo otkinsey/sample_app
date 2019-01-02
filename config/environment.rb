3d0bbd236288faad73979b388f72d3dea589f1c9
okoa kinsey <okoakinsey@okoas-MacBook-Pro.local>
2019-01-01 19:08:18 -0500

    repairing heroku build error

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,9 @@
 # Load the Rails application.
 require_relative 'application'
-config.action_mailer.default_url_options = { :host=>'localhost'}
+
 # Initialize the Rails application.
 Rails.application.initialize!
+
+Rails.application.configure do
+  config.action_mailer.default_url_options = { :host=>'localhost'}
+end

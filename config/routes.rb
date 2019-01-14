19b909bff14910d2d14c8d6573a75440e16f5093
okoa kinsey <okoakinsey@okoas-MacBook-Pro.local>
2019-01-14 16:00:12 -0500

    reviewing chapter 12 unknown progress point. building password reset functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+  get 'password_resets/edit'
   root    'static_pages#home'
   get     '/help',     to:  'static_pages#help'
   get     '/about',    to:  'static_pages#about'
-  get     '/contact',  to:  'static_pages#contact'    
+  get     '/contact',  to:  'static_pages#contact'
   get     '/signup',   to:  'users#new'
   get     '/users',    to:  'users#index'
   get     '/login',    to:  'sessions#new'
@@ -10,4 +12,5 @@ Rails.application.routes.draw do
   delete  '/logout',   to:  'sessions#destroy'
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets, only: [:new, :create, :edit, :update]
 end
